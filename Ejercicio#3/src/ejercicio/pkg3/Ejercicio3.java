51157549e96816b10f297643ee87a6bcabd34e61
Luis Flores <lf016158@gmail.com>
2021-04-06 11:02:28 -0600

    se creo una lista para almacenar los datos de tipo Entero

--- Ejercicio#3/src/ejercicio/pkg3/Ejercicio3.java
+++ Ejercicio#3/src/ejercicio/pkg3/Ejercicio3.java
@@ -5,6 +5,7 @@
  */
 package ejercicio.pkg3;
 
+import java.util.*;
 /**
  *
  * @author Luis Flores
@@ -16,6 +17,36 @@ public class Ejercicio3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        List<Integer> listaNumeros = new ArrayList<Integer>();
+        
+        int numero=1;
+        int acum=0;
+        
+        try
+        {
+           while(numero != 0)
+        {
+          System.out.print("Ingrese un Numero: ");
+          numero = leer.nextInt();
+          acum += numero;
+          if(numero != 0)
+          {
+            listaNumeros.add(numero);
+          }
+         
+        }
+        System.out.println();
+        System.out.println("Estadisticas\n");
+        System.out.println("El máximo valor es: " + Collections.max(listaNumeros));
+        System.out.println("El Minimo valor es: " + Collections.min(listaNumeros));
+        System.out.println("La suma total es: " + acum);
+        }
+        catch(Exception ex)
+        {
+          System.out.println("Error: Solo se Permiten Numeros Enteros\n" + ex);
+        }
+       
     }
     
 }
